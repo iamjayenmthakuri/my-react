33fec91022f17532cf5ea663ef235bcafeb33495
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-12 14:40:34 +0545

    dynamic values in template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog:";
+  const like = 50;
+  const link = "https://www.google.com";
+  // const person ={
+  //   name: 'jayen';
+  //   age: 30
+  // }
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>liked {like} times</p>
+        <p>{10}</p>
+        <p>{"hellow world "}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 20}</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
