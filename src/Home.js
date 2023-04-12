39ce3c7ab369a3f65425dc1491d5bb2add6e2ad5
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-12 15:59:34 +0545

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Home = () => {
-  return <h2>HomePage</h2>;
+  const handleClickAgain = (name) => {
+    console.log("hello" + name);
+  };
+
+  const handleClick = (e) => {
+    console.log("Hello, hinjas", e.target);
+  };
+  return (
+    <div>
+      <h2>HomePage</h2>
+      <button onClick={handleClick}>clickMe</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain(" jayen", e);
+          //   console.log("hello");
+        }}
+      >
+        Click Me again
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -6,16 +6,7 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#f1356d",
-            borderRadius: "*px",
-          }}
-        >
-          New Blog
-        </a>
+        <a href="/create">New Blog</a>
       </div>
     </nav>
   );
