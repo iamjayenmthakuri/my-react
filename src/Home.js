6ea38b776d7e82b4847e9e530d0c6a7eec08b4f6
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-13 10:29:33 +0545

    Conditional loading message -2

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -10,19 +11,23 @@ const Home = () => {
   // };
 
   useEffect(() => {
-    fetch(" http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    setTimeout(() => {
+      fetch(" http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <>
       <div className="home">
+        {isPending && <div> Loading ...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       </div>
     </>
