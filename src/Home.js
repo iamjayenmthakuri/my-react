3fd3c7f76586735c22d6aeb0eac1fc3fa4c2cd81
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-13 12:14:38 +0545

    Making custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,12 @@
-import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch(" http://localhost:8000/blogss")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Something Went wrong: RESPONSE ERROR");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        // connection error
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <>
