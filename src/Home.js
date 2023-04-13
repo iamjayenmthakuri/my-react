c940be51da8df6931260a3db8a4f30e491185c10
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-13 10:55:09 +0545

    Handling Errors

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -12,14 +13,23 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      fetch(" http://localhost:8000/blogs")
+      fetch(" http://localhost:8000/blogss")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("Something Went wrong: RESPONSE ERROR");
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        // connection error
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
@@ -27,6 +37,7 @@ const Home = () => {
   return (
     <>
       <div className="home">
+        {error && <div>{error}</div>}
         {isPending && <div> Loading ...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       </div>
