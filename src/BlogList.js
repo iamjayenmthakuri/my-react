ce2cd327779ccc7c2da8089fd7719d4df72c7841
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-13 10:18:07 +0545

    Conditional Loading message

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   //   const blogs = props.blogs;
   //   const title = props.title;
   return (
@@ -10,7 +10,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by:{blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}></button>
         </div>
       ))}
     </div>
